71c8aef253c5be2ea809c5793c82389be526e4c2
Ojaswa Sharma <93905595+Sowoul@users.noreply.github.com>
2023-01-18 17:47:37 +0530

    Add files via upload

---
+++ swipe.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class swipe : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+
+    public enum Direction { Left, Up, Right, Down, None }
+
+    public Direction direction;
+    public Vector2 startPos, endPos;
+    public float swipeThreshold = 100f;
+    bool draggingStarted;
+
+    private void Awake()
+    {
+        draggingStarted = false;
+        direction = Direction.None;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        draggingStarted = true;
+        startPos = eventData.pressPosition;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (draggingStarted)
+        {
+            endPos = eventData.position;
+
+            Vector2 difference = endPos - startPos; // difference vector between start and end positions.
+
+            if (difference.magnitude > swipeThreshold)
+            {
+                if (Mathf.Abs(difference.x) > Mathf.Abs(difference.y)) // Do horizontal swipe
+                {
+                    Debug.Log("hor");
+                    direction = difference.x > 0 ? Direction.Right : Direction.Left; // If greater than zero, then swipe to right.
+                }
+                else //Do vertical swipe
+                {
+                    direction = difference.y > 0 ? Direction.Up : Direction.Down; // If greater than zero, then swipe to up.
+                }
+            }
+            else
+            {
+                direction = Direction.None;
+            }
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (draggingStarted && direction != Direction.None)
+        {
+            //Do something with this direction data.
+            Debug.Log("Swipe direction: " + direction);
+        }
+
+        //reset the variables
+        startPos = Vector2.zero;
+        endPos = Vector2.zero;
+        draggingStarted = false;
+    }
+
+}
